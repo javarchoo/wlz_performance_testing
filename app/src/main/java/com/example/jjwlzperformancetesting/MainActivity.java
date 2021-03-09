eaee5b7f433d13cd4e77b7815e0b6b850c57d6bd
Jawon Choo <jawon.choo@gmail.com>
2021-03-09 19:04:35 +0900

    calculator test

--- app/src/main/java/com/example/jjwlzperformancetesting/MainActivity.java
+++ app/src/main/java/com/example/jjwlzperformancetesting/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.jjwlzperformancetesting;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +14,46 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void addClick(View v) {
+        EditText number1 = (EditText) findViewById(R.id.number1);
+        EditText number2 = (EditText) findViewById(R.id.number2);
+        TextView result = (TextView) findViewById(R.id.result);
+
+        int n1 = Integer.parseInt(number1.getText().toString());
+        int n2 = Integer.parseInt(number2.getText().toString());
+        result.setText(Integer.toString(n1+n2));
+    }
+
+    public void subtractClick(View v) {
+        EditText number1 = (EditText) findViewById(R.id.number1);
+        EditText number2 = (EditText) findViewById(R.id.number2);
+        TextView result = (TextView) findViewById(R.id.result);
+
+        int n1 = Integer.parseInt(number1.getText().toString());
+        int n2 = Integer.parseInt(number2.getText().toString());
+        result.setText(Integer.toString(n1-n2));
+    }
+
+    public void multiplyClick(View v) {
+        EditText number1 = (EditText) findViewById(R.id.number1);
+        EditText number2 = (EditText) findViewById(R.id.number2);
+        TextView result = (TextView) findViewById(R.id.result);
+
+        int n1 = Integer.parseInt(number1.getText().toString());
+        int n2 = Integer.parseInt(number2.getText().toString());
+        result.setText(Integer.toString(n1*n2));
+    }
+
+    public void devideClick(View v) {
+        EditText number1 = (EditText) findViewById(R.id.number1);
+        EditText number2 = (EditText) findViewById(R.id.number2);
+        TextView result = (TextView) findViewById(R.id.result);
+
+        int n1 = Integer.parseInt(number1.getText().toString());
+        int n2 = Integer.parseInt(number2.getText().toString());
+        result.setText(Integer.toString(n1/n2));
+    }
+
+
 }
