ca4ad85a24cdb22dadad8190f037eb28253eeb6c
Jawon Choo <jawon.choo@gmail.com>
2021-03-10 21:17:28 +0900

    add getLocation add signalStatus

--- app/src/main/java/com/example/jjwlzperformancetesting/MainActivity.java
+++ app/src/main/java/com/example/jjwlzperformancetesting/MainActivity.java
@@ -1,19 +1,34 @@
 package com.example.jjwlzperformancetesting;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.telephony.CellInfo;
+import android.telephony.CellInfoCdma;
+import android.telephony.CellInfoGsm;
+import android.telephony.CellInfoLte;
+import android.telephony.CellInfoNr;
+import android.telephony.CellInfoWcdma;
+import android.telephony.CellSignalStrengthCdma;
+import android.telephony.CellSignalStrengthGsm;
+import android.telephony.CellSignalStrengthLte;
+import android.telephony.CellSignalStrengthNr;
+import android.telephony.CellSignalStrengthWcdma;
+import android.telephony.TelephonyManager;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -22,16 +37,19 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.IOException;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
     private GpsTracker gpsTracker;
+    private static boolean IS_LTE = false;
+    private static boolean IS_5G = false;
 
     private static final int GPS_ENABLE_REQUEST_CODE = 2001;
     private static final int PERMISSIONS_REQUEST_CODE = 100;
-    String[] REQUIRED_PERMISSIONS  = {Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION};
+    String[] REQUIRED_PERMISSIONS = {Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,21 +59,19 @@ public class MainActivity extends AppCompatActivity {
         if (!checkLocationServicesStatus()) {
 
             showDialogForLocationServiceSetting();
-        }else {
+        } else {
 
             checkRunTimePermission();
         }
 
-        final TextView textview_address = (TextView)findViewById(R.id.locationStr);
+        final TextView textview_address = (TextView) findViewById(R.id.locationStr);
         final TextView testview_location = (TextView) findViewById(R.id.locationInfo);
 
 
         Button ShowLocationButton = (Button) findViewById(R.id.getLocation);
-        ShowLocationButton.setOnClickListener(new View.OnClickListener()
-        {
+        ShowLocationButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View arg0)
-            {
+            public void onClick(View arg0) {
                 gpsTracker = new GpsTracker(MainActivity.this);
 
                 double latitude = gpsTracker.getLatitude();
@@ -78,7 +94,7 @@ public class MainActivity extends AppCompatActivity {
 
         int n1 = Integer.parseInt(number1.getText().toString());
         int n2 = Integer.parseInt(number2.getText().toString());
-        result.setText(Integer.toString(n1+n2));
+        result.setText(Integer.toString(n1 + n2));
     }
 
     public void subtractClick(View v) {
@@ -88,7 +104,7 @@ public class MainActivity extends AppCompatActivity {
 
         int n1 = Integer.parseInt(number1.getText().toString());
         int n2 = Integer.parseInt(number2.getText().toString());
-        result.setText(Integer.toString(n1-n2));
+        result.setText(Integer.toString(n1 - n2));
     }
 
     public void multiplyClick(View v) {
@@ -98,7 +114,7 @@ public class MainActivity extends AppCompatActivity {
 
         int n1 = Integer.parseInt(number1.getText().toString());
         int n2 = Integer.parseInt(number2.getText().toString());
-        result.setText(Integer.toString(n1*n2));
+        result.setText(Integer.toString(n1 * n2));
     }
 
     public void devideClick(View v) {
@@ -108,7 +124,7 @@ public class MainActivity extends AppCompatActivity {
 
         int n1 = Integer.parseInt(number1.getText().toString());
         int n2 = Integer.parseInt(number2.getText().toString());
-        result.setText(Integer.toString(n1/n2));
+        result.setText(Integer.toString(n1 / n2));
     }
 
     /*
@@ -119,7 +135,7 @@ public class MainActivity extends AppCompatActivity {
                                            @NonNull String[] permissions,
                                            @NonNull int[] grandResults) {
 
-        if ( permsRequestCode == PERMISSIONS_REQUEST_CODE && grandResults.length == REQUIRED_PERMISSIONS.length) {
+        if (permsRequestCode == PERMISSIONS_REQUEST_CODE && grandResults.length == REQUIRED_PERMISSIONS.length) {
 
             // 요청 코드가 PERMISSIONS_REQUEST_CODE 이고, 요청한 퍼미션 개수만큼 수신되었다면
 
@@ -136,12 +152,11 @@ public class MainActivity extends AppCompatActivity {
             }
 
 
-            if ( check_result ) {
+            if (check_result) {
 
                 //위치 값을 가져올 수 있음
                 ;
-            }
-            else {
+            } else {
                 // 거부한 퍼미션이 있다면 앱을 사용할 수 없는 이유를 설명해주고 앱을 종료합니다.2 가지 경우가 있습니다.
 
                 if (ActivityCompat.shouldShowRequestPermissionRationale(this, REQUIRED_PERMISSIONS[0])
@@ -151,7 +166,7 @@ public class MainActivity extends AppCompatActivity {
                     finish();
 
 
-                }else {
+                } else {
 
                     Toast.makeText(MainActivity.this, "퍼미션이 거부되었습니다. 설정(앱 정보)에서 퍼미션을 허용해야 합니다. ", Toast.LENGTH_LONG).show();
 
@@ -161,7 +176,7 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    void checkRunTimePermission(){
+    void checkRunTimePermission() {
 
         //런타임 퍼미션 처리
         // 1. 위치 퍼미션을 가지고 있는지 체크합니다.
@@ -179,7 +194,6 @@ public class MainActivity extends AppCompatActivity {
 
 
             // 3.  위치 값을 가져올 수 있음
-
 
 
         } else {  //2. 퍼미션 요청을 허용한 적이 없다면 퍼미션 요청이 필요합니다. 2가지 경우(3-1, 4-1)가 있습니다.
@@ -206,7 +220,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    public String getCurrentAddress( double latitude, double longitude) {
+    public String getCurrentAddress(double latitude, double longitude) {
 
         //지오코더... GPS를 주소로 변환
         Geocoder geocoder = new Geocoder(this, Locale.getDefault());
@@ -230,7 +244,6 @@ public class MainActivity extends AppCompatActivity {
         }
 
 
-
         if (addresses == null || addresses.size() == 0) {
             Toast.makeText(this, "주소 미발견", Toast.LENGTH_LONG).show();
             return "주소 미발견";
@@ -238,7 +251,7 @@ public class MainActivity extends AppCompatActivity {
         }
 
         Address address = addresses.get(0);
-        return address.getAddressLine(0).toString()+"\n";
+        return address.getAddressLine(0).toString() + "\n";
 
     }
 
@@ -296,5 +309,123 @@ public class MainActivity extends AppCompatActivity {
 
         return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
                 || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+    }
+
+
+    public void getNetworkName(View v) {
+        // 권한체크, tm.getNetworkType()
+        TelephonyManager tm = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            // TODO - debug
+            System.out.println("권한체크 에러");
+        }
+
+        SignalStatus.getNetworkTypeName(tm.getNetworkType());
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.Q)
+    public void getSignalStatus(View v) {
+
+        String networkType = "";
+        int dbm = 0;
+        int rsrp = 0;
+        int rsrq = 0;
+        int rssi = 0;
+        int rssnr = 0;
+        int csiRsrp = 0;
+        int csiRsrq = 0;
+        int csiSinr = 0;
+        int ssRsrp = 0;
+        int ssRsrq = 0;
+        int ssSinr = 0;
+        String signalStatus = "";
+
+        // 권한체크, tm.getNetworkType()
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            // TODO - debug
+            System.out.println("권한체크 에러");
+            return;
+        }
+
+        // 권한체크, tm.getAllCellInfo()
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            // TODO - debug
+            System.out.println("권한체크 에러");
+            return;
+        }
+
+        TelephonyManager tm = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+
+        networkType = SignalStatus.getNetworkTypeName(tm.getNetworkType());
+        TextView networkMode = (TextView) findViewById(R.id.networkMode);
+        networkMode.setText(networkType);
+
+        List<CellInfo> cellInfoList = tm.getAllCellInfo();
+        // TODO - debug
+        System.out.println("@@@@  " + cellInfoList.size());
+
+        if (cellInfoList != null) {
+            for (int i = 0; i < cellInfoList.size(); i++) {
+                if (cellInfoList.get(i).isRegistered()) {
+                    if (cellInfoList.get(i) instanceof CellInfoWcdma) {
+                        CellInfoWcdma cellInfoWcdma = (CellInfoWcdma) cellInfoList.get(i);
+                        CellSignalStrengthWcdma cs = cellInfoWcdma.getCellSignalStrength();
+                        dbm = cs.getDbm();
+                    } else if (cellInfoList.get(i) instanceof CellInfoGsm) {
+                        CellInfoGsm cellInfogsm = (CellInfoGsm) cellInfoList.get(i);
+                        CellSignalStrengthGsm cs = cellInfogsm.getCellSignalStrength();
+                        dbm = cs.getDbm();
+                    } else if (cellInfoList.get(i) instanceof CellInfoCdma) {
+                        CellInfoCdma cellInfoCdma = (CellInfoCdma) cellInfoList.get(i);
+                        CellSignalStrengthCdma cs = cellInfoCdma.getCellSignalStrength();
+                        dbm = cs.getDbm();
+                    } else if (cellInfoList.get(i) instanceof CellInfoLte) {
+                        System.out.println("@@@@@@@@LTE");
+                        CellInfoLte cellInfoLte = (CellInfoLte) cellInfoList.get(i);
+                        CellSignalStrengthLte cs = cellInfoLte.getCellSignalStrength();
+                        dbm = cs.getDbm();
+                        signalStatus = signalStatus.concat("RSRP/RSRQ/Rssi/Rssnr: ").
+                                concat(cs.getRsrp() + "/" + cs.getRsrq() + "/" + cs.getRssi() + "/" + cs.getRssnr());
+                    } else if (cellInfoList.get(i) instanceof CellInfoNr) {
+                        System.out.println("@@@@@@@@NR");
+                        CellInfoNr cellInfoNr = (CellInfoNr) cellInfoList.get(i);
+                        CellSignalStrengthNr cs = (CellSignalStrengthNr) cellInfoNr.getCellSignalStrength();
+                        dbm = cs.getDbm();
+                        signalStatus = signalStatus.concat("CSI RSRP/RSRQ/SINR, SS RSRP/RSRQ/SINR: ")
+                                .concat(cs.getCsiRsrp() + "/" + cs.getCsiRsrq() + "/" + cs.getCsiSinr())
+                                .concat("SS RSRP/RSRQ/SINR: ")
+                                .concat(cs.getSsRsrp() + "/" + cs.getSsRsrq() + "/" + cs.getSsSinr());
+                    }
+                }
+            }
+        }
+
+        TextView signalStrength = (TextView) findViewById(R.id.signalStrength);
+        signalStrength.setText(String.valueOf("Signal Strength" + dbm));
+
+        System.out.println("Signal Status :" + signalStatus);
+        TextView signalStatusStr = (TextView) findViewById(R.id.signalStatusStr);
+        signalStatusStr.setText(signalStatus);
     }
 }
